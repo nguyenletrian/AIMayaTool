ca331d0653461179d3098ab91c389713226ecdbe
nguyenletrian <68546373+nguyenletrian@users.noreply.github.com>
2026-09-20 11:41:07 +0700

    Group skin influence mutations into single undo steps

--- aimayatool/maya/skin.py
+++ aimayatool/maya/skin.py
@@ -4,6 +4,8 @@ import time
 
 import maya.cmds as cmds
 
+from .undo import undo_chunk
+
 
 def mesh_from_component(node):
     return (node or "").split(".", 1)[0]
@@ -51,30 +53,32 @@ def skin_data_many(nodes):
 def add_influences(skin_cluster, joints, weight=0.0, lock_weights=True):
     current = set(influences(skin_cluster))
     added = []
-    for joint in joints or []:
-        if joint in current or not cmds.objExists(joint):
-            continue
-        cmds.skinCluster(
-            skin_cluster,
-            edit=True,
-            addInfluence=joint,
-            weight=weight,
-            lockWeights=lock_weights,
-        )
-        current.add(joint)
-        added.append(joint)
+    with undo_chunk('AIMayaTool Add Skin Influences'):
+        for joint in joints or []:
+            if joint in current or not cmds.objExists(joint):
+                continue
+            cmds.skinCluster(
+                skin_cluster,
+                edit=True,
+                addInfluence=joint,
+                weight=weight,
+                lockWeights=lock_weights,
+            )
+            current.add(joint)
+            added.append(joint)
     return added
 
 
 def remove_influences(skin_cluster, joints):
     current = set(influences(skin_cluster))
     removed = []
-    for joint in joints or []:
-        if joint not in current:
-            continue
-        cmds.skinCluster(skin_cluster, edit=True, removeInfluence=joint)
-        current.discard(joint)
-        removed.append(joint)
+    with undo_chunk('AIMayaTool Remove Skin Influences'):
+        for joint in joints or []:
+            if joint not in current:
+                continue
+            cmds.skinCluster(skin_cluster, edit=True, removeInfluence=joint)
+            current.discard(joint)
+            removed.append(joint)
     return removed
 
 
